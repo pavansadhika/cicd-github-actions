cef6c6bba5e081215113baee0cb2000d1827594b
pavansadhika <121276288+pavansadhika@users.noreply.github.com>
2023-02-14 15:43:52 +0530

    update

--- src/test/java/com/javatechie/cicd/GithubCicdActionsApplicationTests.java
+++ src/test/java/com/javatechie/cicd/GithubCicdActionsApplicationTests.java
@@ -2,12 +2,31 @@ package com.javatechie.cicd;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import java.util.concurrent.TimeUnit;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import io.github.bonigarcia.wdm.WebDriverManager;
 
-@SpringBootTest
-class GithubCicdActionsApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
+public class GithubCicdActionsApplicationTests 
+{ 
+	public WebDriver driver; 
+	public String baseUrl = "https://www.lambdatest.com/";  
+	
+	@Test             
+	public void contextLoads() {      
+		
+		WebDriverManager.chromedriver().setup();
+		ChromeOptions options = new ChromeOptions();
+		options.addArguments("--no-sandbox");
+		options.addArguments("--disable-dev-shm-usage");
+		options.addArguments("--headless");
+		driver = new ChromeDriver(options);
+ 
+		driver.manage().timeouts().implicitlyWait(20,TimeUnit.SECONDS);  
+		driver.manage().window().maximize();  
+		driver.get(baseUrl);
+		driver.close();
+	}     
 }
